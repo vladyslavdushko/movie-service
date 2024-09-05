0d9155d4887acc45d225fbd5a084905f10f39c2a
Vladyslav Dushko <jafter@ukr.net>
2024-09-05 13:26:38 +0300

    styles

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -2,15 +2,19 @@ import { useDispatch, useSelector } from 'react-redux';
 import styles from './UserMenu.module.css';
 import { selectEmail, selectName } from '../../redux/firebaseAuth/selectors';
 import { signOutUser } from '../../redux/firebaseAuth/operations';
+import logOut from '../../images/logOut.svg';
 const UserMenu = () => {
   const email = useSelector(selectEmail);
   const name = useSelector(selectName);
-  // const token = useSelector(selectToken);
   const dispatch = useDispatch();
+
   return (
     <div className={styles.user_nav_item}>
       <p>Welcome, {name ? name : email} 😎</p>
-      <button onClick={() => dispatch(signOutUser())}>Signout</button>
+      <button className={styles.signout_button} onClick={() => dispatch(signOutUser())}>
+        <img src={logOut} alt="logout-icon" />
+        Sign out
+      </button>
     </div>
   );
 };
